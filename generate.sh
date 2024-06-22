cfd34fd318c7aacf580322080724346db2237559
balhafni <ba63@nyu.edu>
2024-06-22 12:34:38 +0400

    Generation script

---
+++ generate.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+#SBATCH -p nvidia
+#SBATCH -q nlp
+# use gpus
+#SBATCH --gres=gpu:a100:1
+# memory
+#SBATCH --mem=220GB
+# Walltime format hh:mm:ss
+#SBATCH --time=40:00:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
+
+sys=/scratch/ba63/coda-did/models/t5/gip
+test_file=/scratch/ba63/coda-did/sources/test/gip_pred.json
+pred_file=gip_pred.test.gen
+
+printf "Running inference on ${test_file}\n"
+printf "Generating outputs using: ${sys}\n"
+
+python generate.py \
+        --model_name_or_path $sys \
+        --source_lang src \
+        --target_lang tgt \
+        --test_file $test_file \
+        --per_device_eval_batch_size 16 \
+        --output_dir $sys \
+        --num_beams 5 \
+        --num_return_sequences 1 \
+        --max_target_length 200 \
+        --predict_with_generate \
+        --prediction_file $pred_file
+
+
+# separated generate
+# for x in rab bei cai tun doh
+# do
+#         sys=/scratch/ba63/coda-did/models/t5/separated/$x
+#         for y in gold pred
+#         do 
+#                 test_file=/scratch/ba63/coda-did/sources/dev/separated/dev_${y}.${x^^}.json
+#                 pred_file=${x^^}_${y}.gen
+
+#                 printf "Running inference on ${test_file}\n"
+#                 printf "Generating outputs using: ${sys}\n"
+
+#                 python generate.py \
+#                         --model_name_or_path $sys \
+#                         --source_lang src \
+#                         --target_lang tgt \
+#                         --test_file $test_file \
+#                         --per_device_eval_batch_size 16 \
+#                         --output_dir $sys \
+#                         --num_beams 5 \
+#                         --num_return_sequences 1 \
+#                         --max_target_length 200 \
+#                         --predict_with_generate \
+#                         --prediction_file $pred_file
+
+#         done
+
+
+# done
